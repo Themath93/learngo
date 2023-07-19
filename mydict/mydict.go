16f10708001780e92ca95e7356c65a7886deaca1
Yoon <lms4678@naver.com>
2023-07-20 02:17:03 +0900

    230720 go learn small Dictionary Application

---
+++ mydict/mydict.go
@@ -0,0 +1,63 @@
+package mydict
+
+import "errors"
+
+// Dictionary type
+type Dictionary map[string]string
+
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant update non-existing word")
+	errWordExists = errors.New("That word already in Dictionary")
+	errCantDelete = errors.New("Cant delete non-existing word")
+)
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update definition for exists word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case err:
+		return errCantUpdate
+	}
+	return nil
+
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		delete(d, word)
+	case err:
+		return errCantDelete
+	}
+	return nil
+
+}
